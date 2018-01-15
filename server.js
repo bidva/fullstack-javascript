566eab8aea6e0b9481a668248e227e3318a2bf8c
amir bidva <a.bidva@gmail.com>
2018-01-14 23:30:23 -0500

    adding node server

---
+++ server.js
@@ -0,0 +1,19 @@
+import config from './config.js';
+import apiRouter from "./api";
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/',(req, res) => {
+  res.send('Hello Express')
+})
+
+server.use('/api',apiRouter);
+
+server.use(express.static('public'));
+
+server.listen(config.port,()=>{
+  console.info('Express listening on port ',config.port);
+})
