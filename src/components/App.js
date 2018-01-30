8eddb9ff34d22cf73073e73733033532860f0b50
amir bidva <a.bidva@gmail.com>
2018-01-30 11:17:03 -0500

    accept a Matrix and identify if it is a sparse matrix or not

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import PropTypes from 'prop-types';
 import ContestList from './ContestList';
+import Contest from './Contest';
 
 const pushState = (obj, url) =>{
   window.history.pushState(obj, '', url);
@@ -17,21 +18,31 @@ class App extends React.Component {
     pushState(
       { currentContestId: contestId},
       `/contest/${contestId}`);
+    this.setState({
+      pageHeader: this.state.contests[contestId].contestName,
+      currentContestId: contestId
+    });
+  }
+  currentContent(){
+    if (this.state.currentContestId) {
+      return <Contest {...this.state.contests[this.state.currentContestId]} />;
+    }
+    return <ContestList
+      onContestClick={this.fetchContest}
+      contests={this.state.contests} />;
   }
   render(){
     return(
       <div className="App">
         <Header message={this.state.pageHeader}/>
-        <ContestList
-          onContestClick={this.fetchContest}
-          contests={this.state.contests} />
+        {this.currentContent()}
       </div>
     );
   }
 }
 
 App.propTypes = {
-  data: PropTypes.array
+  data: PropTypes.object
 };
 
 export default App;
